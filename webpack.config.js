9841c265ec8ded5c9c061289d87e774e7d4fcca9
LeoJ340 <1015668003@qq.com>
2025-05-21 10:53:57 +0800

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,87 @@
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 const title = 'webpack-template'
 
 module.exports = env => {
-    const isProd = env['production']
-    return {
-        mode: isProd ? 'production' : 'development',
-        devtool: isProd ? 'source-map' : 'cheap-module-source-map',
-        entry: {
-            app: './src/index.js'
-        },
-        output: {
-            filename: `static/js/[name].[contenthash].js`,
-            clean: true
-        },
-        module: {
-            rules: [
-                {
-                    oneOf: [
-                        {
-                            test: /\.css$/,
-                            use: [ MiniCssExtractPlugin.loader, 'css-loader'],
-                            exclude: path.resolve(__dirname, 'src')
-                        },
-                        {
-                            test: /\.(png|svg|jpe?g|gif|webp)$/i,
-                            type: 'asset',
-                            generator: {
-                                filename: 'static/[hash][ext][query]'
-                            },
-                        },
-                        {
-                            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                            type: 'asset/resource',
-                            generator: {
-                                filename: 'static/[hash][ext][query]'
-                            }
-                        },
-                        {
-                            test: /\.js$/,
-                            include: path.resolve(__dirname, 'src'),
-                            use: ['babel-loader'],
-                        },
-                    ]
-                }
-            ]
-        },
-        optimization: {
-            minimizer: [
-                new CssMinimizerPlugin(),
-            ],
-            splitChunks: {
-                chunks: "all"
+  const isProd = env.production
+  return {
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'source-map' : 'cheap-module-source-map',
+    entry: {
+      app: './src/index.js'
+    },
+    output: {
+      filename: 'static/js/[name].[contenthash].js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          oneOf: [
+            {
+              test: /\.css$/,
+              use: [MiniCssExtractPlugin.loader, 'css-loader'],
+              exclude: path.resolve(__dirname, 'src/components') // 排除组件内的样式
             },
-            runtimeChunk: {
-                name: 'runtime',
+            {
+              test: /\.(png|svg|jpe?g|gif|webp)$/i,
+              type: 'asset',
+              generator: {
+                filename: 'static/[hash][ext][query]'
+              }
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: 'static/[hash][ext][query]'
+              }
+            },
+            {
+              test: /\.js$/,
+              include: path.resolve(__dirname, 'src'),
+              use: ['babel-loader']
             }
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title,
-                template: path.resolve(__dirname, 'index.html')
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'static/css/[name].[contenthash].css'
-            }),
-            new ESLintPlugin({
-                context: path.resolve(__dirname, 'src')
-            }),
-        ],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, 'src/'),
-                '@components': path.resolve(__dirname, 'src/components/')
-            }
-        },
-        devServer: {
-            port: 88,
-            open: true
+          ]
         }
-    };
-};
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new CssMinimizerPlugin()
+      ],
+      splitChunks: {
+        chunks: 'all'
+      },
+      runtimeChunk: {
+        name: 'runtime'
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title,
+        template: path.resolve(__dirname, 'index.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'static/css/[name].[contenthash].css'
+      }),
+      new ESLintPlugin({
+        context: path.resolve(__dirname, 'src')
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        '@components': path.resolve(__dirname, 'src/components/')
+      }
+    },
+    devServer: {
+      port: 88,
+      open: true
+    }
+  }
+}
